f05fd80e4c486fff9e4ba83159d08ff1206bf257
Dipanjan Das <dasdipanjan04@gmail.com>
2024-11-28 22:54:29 +0100

    Update the main.py accordingly.

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from src.pipeline.pipeline import preprocess, inference, postprocess
 app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
 
 @app.get("/health")
 async def health_check():
     return {"status" : "healthy"}
 
 @app.post("/query")
-async def process_query(query: str):
-    return {"response" : f"Test Query is {query}"}
+async def process_query(request: QueryRequest):
+    preprocessed_query = preprocess(request.query)
+    model_response = inference(preprocessed_query)
+    final_response = postprocess(model_response)
+    return {"response" : final_response}
+
