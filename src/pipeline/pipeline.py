e06d0d559fa146c2da28358ac6d924e91c8ca801
Dipanjan Das <dasdipanjan04@gmail.com>
2024-11-28 22:53:09 +0100

    Basic template for preprocessing the input text, inference and final post processed response from the model.

---
+++ src/pipeline/pipeline.py
@@ -0,0 +1,9 @@
+def preprocess(text: str):
+    return text
+
+def inference(text: str):
+    inference_response: str = "Test"
+    return f"Generated Response for the given {text}: {inference_response}"
+
+def postprocess(text: str):
+    return text
