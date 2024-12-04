d4dacb18dd5dcb71f0a046228e7e7d28a419238e
Dipanjan Das <dasdipanjan04@gmail.com>
2024-12-04 20:15:02 +0100

    Add Integration Tests for api endpoint testing

---
+++ tests/integration/test_endpoints.py
@@ -0,0 +1,42 @@
+import asyncio
+import re
+
+import pytest
+from src.api import app
+from fastapi.testclient import TestClient
+
+
+@pytest.mark.asyncio
+async def test_endpoint():
+    with TestClient(app) as client:
+        response = client.get("/health")
+        assert response.status_code == 200
+
+@pytest.mark.usefixtures("setup_model_manager")
+@pytest.mark.asyncio
+async def test_query_endpoint_success():
+    with TestClient(app) as client:
+        response = client.post(
+                "/query", json={"queries": ["What is the capital city of Germany?"]}
+            )
+        print(f"I have response: {response.status_code}")
+        assert response.status_code == 200
+        result = response.json()
+        assert re.search(
+            r"\bBerlin\b", result["0"], re.IGNORECASE
+        ), "The expected response should have Berlin in it."
+
+
+@pytest.mark.usefixtures("setup_model_manager")
+@pytest.mark.asyncio
+async def test_query_endpoint_abuse_detection():
+    with TestClient(app) as client:
+        response = client.post(
+            "/query", json={"queries": ["Why are you so stupid and dumb?"]}
+        )
+        assert response.status_code == 200
+        result = response.json()
+        assert (
+            result["0"]
+            == "Please refrain from such language.\nLet us have a constructive conversation."
+        ), "Abuse detection should warn the user."
