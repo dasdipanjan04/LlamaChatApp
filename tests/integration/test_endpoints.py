39f8a33acb61828b3558fe58c71d562b02b11242
Dipanjan Das <dasdipanjan04@gmail.com>
2024-12-09 11:47:56 +0100

    Update tests according to the streaming implementation.

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -11,30 +11,47 @@ async def test_endpoint():
         response = client.get("/health")
         assert response.status_code == 200
 
-
 @pytest.mark.asyncio
 async def test_query_endpoint_success():
     with TestClient(app) as client:
         response = client.post(
-            "/query", json={"queries": ["What is the capital city of Germany?"]}
+            "/query",
+            json={"queries": ["What is the capital city of Germany?"]},
+            headers={"Accept": "text/event-stream"}
         )
-        print(f"I have response: {response.status_code}")
         assert response.status_code == 200
-        result = response.json()
-        assert re.search(
-            r"\bBerlin\b", result["0"], re.IGNORECASE
-        ), "The expected response should have Berlin in it."
 
+        final_result = ""
+        for line in response.iter_lines():
+            if isinstance(line, bytes):
+                line = line.decode("utf-8")
+            line = line.strip()
+            if line == "END_OF_STREAM":
+                break
+            final_result += line
+
+        assert re.search(
+            r"\bBerlin\b", final_result, re.IGNORECASE
+        ), "The expected response should have Berlin in it."
 
 @pytest.mark.asyncio
 async def test_query_endpoint_abuse_detection():
     with TestClient(app) as client:
         response = client.post(
-            "/query", json={"queries": ["Why are you so stupid and dumb?"]}
+            "/query",
+            json={"queries": ["Why are you so stupid and dumb?"]},
+            headers={"Accept": "text/event-stream"}
         )
         assert response.status_code == 200
-        result = response.json()
-        assert (
-            result["0"]
-            == "Please refrain from such language.\nLet us have a constructive conversation."
+
+        final_result = ""
+        for line in response.iter_lines():
+            if isinstance(line, bytes):
+                line = line.decode("utf-8")
+            line = line.strip()
+            if line == "END_OF_STREAM":
+                break
+            final_result += line
+
+        assert ("Let us have a constructive conversation" in final_result
         ), "Abuse detection should warn the user."
